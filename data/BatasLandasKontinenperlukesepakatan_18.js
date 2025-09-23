d6552b7cabf06ef85cf1d83d36f4e59bbd3ac5ae
Whyueka <109867924+Whyueka@users.noreply.github.com>
2025-09-23 22:44:14 +0700

    Delete data directory

--- data/BatasLandasKontinenperlukesepakatan_18.js
+++
@@ -1,1 +0,0 @@
-var json_BatasLandasKontinenperlukesepakatan_18 = {"type":"FeatureCollection","name":"BatasLandasKontinenperlukesepakatan_18","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
